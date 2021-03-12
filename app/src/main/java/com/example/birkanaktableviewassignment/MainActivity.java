006a6919ff7a8c7d4922a9caf2bbaa222a1b10f4
BirkanAk <bak1@binghamton.edu>
2021-03-12 21:59:48 +0300

    All done! Works perfectly, no bugs with removing too!

--- app/src/main/java/com/example/birkanaktableviewassignment/MainActivity.java
+++ app/src/main/java/com/example/birkanaktableviewassignment/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.birkanaktableviewassignment;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
@@ -15,10 +17,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView tulkas;
-    ImageView ulmo;
-    ImageView mandos;
-    ImageView melkor;
 
     private TableLayout table_layout;
     private ArrayList<TableRow> tableRowList;
@@ -31,65 +29,137 @@ public class MainActivity extends AppCompatActivity {
 
         tableRowList=new ArrayList<>();
         table_layout = (TableLayout)findViewById(R.id.table_layout);
-
-        tulkas=(ImageView)findViewById(R.id.imageView1);
-        ulmo=(ImageView)findViewById(R.id.imageView2);
-        mandos=(ImageView)findViewById(R.id.imageView3);
-        melkor=(ImageView)findViewById(R.id.imageView4);
     }
 
     public void add_melkor(View view) {
-        TableRow tableRow;
-        tableRow = new TableRow(this);
-        ImageView im = new ImageView(this);
-        im.setImageDrawable(getResources().getDrawable(R.drawable.melkor));
-        tableRow.addView(im,127,136);
-        TextView text = new TextView(this);
-        text.setText("You have picked Melkor, the God of Corruption!");
-        tableRow.addView(text,1);
-        table_layout.addView(tableRow);
-        tableRowList.add(tableRow);
+        if (tableRowList.size()>=2){
+            AlertDialog dialog1 = new AlertDialog.Builder(MainActivity.this).create();
+            dialog1.setTitle("This is a Duo Team!!!");
+            dialog1.setMessage("You cannot have more than 2 gods in your team. Please remove and then add again if you want that god in your team!");
+            dialog1.setButton(AlertDialog.BUTTON_NEUTRAL, "Yes I'll remove a god.",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            dialog1.show();
+        }
+        else {
+            TableRow tableRow;
+            tableRow = new TableRow(this);
+            ImageView im = new ImageView(this);
+            im.setImageDrawable(getResources().getDrawable(R.drawable.melkor));
+            tableRow.addView(im,127,136);
+            TextView text = new TextView(this);
+            text.setText("You have picked Melkor, the God of Corruption!");
+            tableRow.addView(text,1);
+            table_layout.addView(tableRow);
+            tableRowList.add(tableRow);
+        }
+
     }
 
     public void add_mandos(View view) {
-        TableRow tableRow;
-        tableRow = new TableRow(this);
-        ImageView im = new ImageView(this);
-        im.setImageDrawable(getResources().getDrawable(R.drawable.mandos));
-        tableRow.addView(im,127,136);
-        TextView text = new TextView(this);
-        text.setText("You have picked Mandos, the God of the Dead!");
-        tableRow.addView(text,1);
-        table_layout.addView(tableRow);
-        tableRowList.add(tableRow);
+        if (tableRowList.size()>=2){
+            AlertDialog dialog1 = new AlertDialog.Builder(MainActivity.this).create();
+            dialog1.setTitle("This is a Duo Team!!!");
+            dialog1.setMessage("You cannot have more than 2 gods in your team. Please remove and then add again if you want that god in your team!");
+            dialog1.setButton(AlertDialog.BUTTON_NEUTRAL, "Yes I'll remove a god.",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            dialog1.show();
+        }
+        else {
+            TableRow tableRow;
+            tableRow = new TableRow(this);
+            ImageView im = new ImageView(this);
+            im.setImageDrawable(getResources().getDrawable(R.drawable.mandos));
+            tableRow.addView(im,127,136);
+            TextView text = new TextView(this);
+            text.setText("You have picked Mandos, the God of the Dead!");
+            tableRow.addView(text,1);
+            table_layout.addView(tableRow);
+            tableRowList.add(tableRow);
+        }
+
     }
 
     public void add_ulmo(View view) {
-        TableRow tableRow;
-        tableRow = new TableRow(this);
-        ImageView im = new ImageView(this);
-        im.setImageDrawable(getResources().getDrawable(R.drawable.ulmo));
-        tableRow.addView(im,127,136);
-        TextView text = new TextView(this);
-        text.setText("You have picked Ulmo, the God of Seas!");
-        tableRow.addView(text,1);
-        table_layout.addView(tableRow);
-        tableRowList.add(tableRow);
+        if (tableRowList.size()>=2){
+            AlertDialog dialog1 = new AlertDialog.Builder(MainActivity.this).create();
+            dialog1.setTitle("This is a Duo Team!!!");
+            dialog1.setMessage("You cannot have more than 2 gods in your team. Please remove and then add again if you want that god in your team!");
+            dialog1.setButton(AlertDialog.BUTTON_NEUTRAL, "Yes I'll remove a god.",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            dialog1.show();
+        }
+        else {
+            TableRow tableRow;
+            tableRow = new TableRow(this);
+            ImageView im = new ImageView(this);
+            im.setImageDrawable(getResources().getDrawable(R.drawable.ulmo));
+            tableRow.addView(im,127,136);
+            TextView text = new TextView(this);
+            text.setText("You have picked Ulmo, the God of Seas!");
+            tableRow.addView(text,1);
+            table_layout.addView(tableRow);
+            tableRowList.add(tableRow);
+        }
+
     }
 
     public void add_tulkas(View view) {
-        TableRow tableRow;
-        tableRow = new TableRow(this);
-        ImageView im = new ImageView(this);
-        im.setImageDrawable(getResources().getDrawable(R.drawable.tulkas));
-        tableRow.addView(im,127,136);
-        TextView text = new TextView(this);
-        text.setText("You have picked Tulkas, the God of Strength!");
-        tableRow.addView(text,1);
-        table_layout.addView(tableRow);
-        tableRowList.add(tableRow);
+        if (tableRowList.size()>=2){
+            AlertDialog dialog1 = new AlertDialog.Builder(MainActivity.this).create();
+            dialog1.setTitle("This is a Duo Team!!!");
+            dialog1.setMessage("You cannot have more than 2 gods in your team. Please remove and then add again if you want that god in your team!");
+            dialog1.setButton(AlertDialog.BUTTON_NEUTRAL, "Yes I'll remove a god.",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            dialog1.show();
+        }
+        else {
+            TableRow tableRow;
+            tableRow = new TableRow(this);
+            ImageView im = new ImageView(this);
+            im.setImageDrawable(getResources().getDrawable(R.drawable.tulkas));
+            tableRow.addView(im,127,136);
+            TextView text = new TextView(this);
+            text.setText("You have picked Tulkas, the God of Strength!");
+            tableRow.addView(text,1);
+            table_layout.addView(tableRow);
+            tableRowList.add(tableRow);
+        }
+
     }
 
     public void remove_button(View view) {
+        if(tableRowList.size()==0){
+            AlertDialog dialog1 = new AlertDialog.Builder(MainActivity.this).create();
+            dialog1.setTitle("You can't remove a god from your team if you didn't add any!");
+            dialog1.setMessage("I know what you're doing. You're trying to break my program! I won't let you!");
+            dialog1.setButton(AlertDialog.BUTTON_NEUTRAL, "Haha you got me...",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            dialog1.show();
+        }
+        else{
+            TableRow row = tableRowList.remove(tableRowList.size()-1);
+            table_layout.removeView(row);
+        }
+
     }
 }
